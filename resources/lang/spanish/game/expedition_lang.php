e12e3d034ca6951fd422bdbd437c65bd5287cce5
Kas-Akari <37886456+Kas-Akari@users.noreply.github.com>
2026-01-20 22:39:04 +0100

    Added new spanish phrases for the expedition cases

--- resources/lang/spanish/game/expedition_lang.php
+++ resources/lang/spanish/game/expedition_lang.php
@@ -8,46 +8,46 @@ $lang = [
     'exp_new_ships_2' => '¡Hemos encontrado restos de una expedición previa! Nuestros técnicos intentarán conseguir que algunas de las naves funcionen de nuevo.<br><br>Las siguientes naves son ahora parte de la flota:<br>%s', // ok
     'exp_new_ships_3' => 'Nuestra expedición se encontró con un antiguo hangar automático. Algunas de las naves se encuentran todavía en la fase de producción y nuestros técnicos están tratando de reactivar los generadores de energía.<br><br>Las siguientes naves son ahora parte de la flota:<br>%s', // ok
     'exp_new_ships_4' => 'Encontramos una estación pirata desierta. En el hangar hay estacionadas algunas naves antiguas. Nuestros técnicos están mirando si algunas de ellas son aún útiles.<br><br>Las siguientes naves son ahora parte de la flota:<br>%s', // ok
-    'exp_new_ships_5' => '',
-    'exp_new_ships_6' => '',
-    'exp_new_ships_7' => '',
+    'exp_new_ships_5' => 'La expedición detectó una nave abandonada en una órbita inusual. Tras investigar, nuestros técnicos lograron reactivar varias naves y añadirlas a la flota.<br><br>Las siguientes naves son ahora parte de la flota:<br>%s',
+    'exp_new_ships_6' => 'Un campo de escombros ocultaba varias naves intactas. Los ingenieros han trabajado intensamente para ponerlas en funcionamiento y unirlas a la expedición.<br><br>Las siguientes naves son ahora parte de la flota:<br>%s',
+    'exp_new_ships_7' => 'En una estación espacial olvidada con el logotipo de BurSen, la expedición encontró hangares llenos de naves en estado cochambroso pero algunas son reparables. Tras una revisión, algunas han sido incorporadas a la flota.<br><br>Las siguientes naves son ahora parte de la flota:<br>%s',
 
     // found resources
     'exp_new_resources_1' => 'En un planetoide abondonado han sido encontradas algunas zonas de recursos fácilmente accesibles y se han recolectado allí algunos recursos satisfactoriamente.<br><br>Se ha capturado %s de %s.', // ok
     'exp_new_resources_2' => 'Tu expedición descubrió un pequeño asteroide en el cual se pueden reciclar algunos recursos.<br><br>Se ha capturado %s de %s.', // ok
-    'exp_new_resources_3' => '',
-    'exp_new_resources_4' => '',
+    'exp_new_resources_3' => 'La expedición encontró una mina automatizada abandonada marca BurSen. Tras una breve inspección del artefacto titulado Liberador de Zumo, se logró extraer recursos valiosos.<br><br>Se ha capturado %s de %s.',
+    'exp_new_resources_4' => 'Un campo de asteroides ocultaba depósitos de recursos sin explotar. Los equipos de la expedición han recolectado lo que han podido.<br><br>Se ha capturado %s de %s.',
 
     // found primitive enemy
     'exp_primitive_enemy_1' => 'Algunos bárbaros primitivos están atacándonos con naves espaciales que no deberían ser llamadas como tal. Si nos disparan en serio, nos veremos forzados a devolver el fuego.', // ok
     'exp_primitive_enemy_2' => 'La señal de emergencia que la expedición siguió, resultó ser un montaje de algunos bucaneros estelares. La batalla no se pudo evitar.', // ok
     'exp_primitive_enemy_3' => 'Capturamos algunos mensajes de radio de algunos piratas borrachos. Pareciera como si nos quisieran atacar.', // ok
     'exp_primitive_enemy_4' => 'Tuvimos que luchar contra algunos piratas que por suerte no eran muchos.', // ok
-    'exp_primitive_enemy_5' => '',
-    'exp_primitive_enemy_6' => '',
-    'exp_primitive_enemy_7' => '',
-    'exp_primitive_enemy_8' => '',
+    'exp_primitive_enemy_5' => 'Un grupo de saqueadores del Zinc Campeador con naves improvisadas intentó emboscar a la expedición. Tras un breve enfrentamiento, logramos repeler el ataque.',
+    'exp_primitive_enemy_6' => 'La expedición fue interceptada por piratas que usaban tecnología obsoleta, probablemente todavía basada en el Boeing 737. La batalla fue corta y conseguimos defendernos sin grandes daños gracias a su tecnologia anticuada de los años 60.',
+    'exp_primitive_enemy_7' => 'Unos bandidos espaciales intentaron abordar la flota usando cápsulas rudimentarias. La seguridad de la expedición actuó rápidamente y evitó el peligro.',
+    'exp_primitive_enemy_8' => 'La expedición detectó señales hostiles provenientes de una nave oxidada. Los sistemas de defensa respondieron a la emboscada y la amenaza fue neutralizada tras un breve enfrentamiento.',
 
     // found advanced enemy
     'exp_advanced_enemy_1' => '¡Algunas naves de apariencia exótica atacaron la expedición sin previo aviso!', // ok
-    'exp_advanced_enemy_2' => '',
+    'exp_advanced_enemy_2' => 'La expedición fue sorprendida por una flota de naves desconocidas equipadas con tecnología avanzada. El enfrentamiento fue intenso y la flota apenas logró escapar.',
 
     // found trader
     'exp_trader' => 'Tu flota en expedición tuvo un corto contacto con una raza de aliens un poco vergonzosa. Estos anunciaron que van a enviar a un representante con bienes de comercio a tus mundos.', // ok
 
     // found dark matter
     'exp_dm_1' => 'La expedición siguió algunas señales fuera de lo común y descrubrió un asteroide. En el núcle del asteroide se encontraba una pequeña cantidad de Materia Oscura. El asteroide fue transportado a bordo y los exploradores intentan extrar la Materia Oscura.<br><br>Se ha capturado %s de %s.', // ok
-    'exp_dm_2' => '',
-    'exp_dm_3' => '',
-    'exp_dm_4' => '',
-    'exp_dm_5' => '',
+    'exp_dm_2' => 'La expedición detectó una anomalía gravitacional y, tras investigar, se encontró una pequeña cápsula de Materia Oscura flotando en el vacío.<br><br>Se ha capturado %s de %s.',
+    'exp_dm_3' => 'Un campo de energía desconocido rodeaba una nube de Materia Oscura. Los científicos lograron extraer una cantidad significativa para su estudio.<br><br>Se ha capturado %s de %s.',
+    'exp_dm_4' => 'En las profundidades de una nebulosa, la expedición halló fragmentos de Materia Oscura adheridos a restos de antiguas naves.<br><br>Se ha capturado %s de %s.',
+    'exp_dm_5' => 'Un artefacto alienígena contenía un núcleo de Materia Oscura. Tras un delicado proceso de extracción, la expedición consiguió recuperarlo.<br><br>Se ha capturado %s de %s.',
 
     // delay
     'exp_delay_1' => 'Tu expedición entro en un sector lleno de tormentas de partículas. Esto provocó que los depósitos de energía se sobrecargaran y la mayoría de los sistemas principales de las naves se averiaron. Tus mecánicos fueron capaces de evitar lo peor pero la flota va a volver con un gran retraso.', // ok
-    'exp_delay_2' => '',
-    'exp_delay_3' => '',
-    'exp_delay_4' => '',
-    'exp_delay_5' => '',
+    'exp_delay_2' => 'Una anomalía temporal afectó los sistemas de navegación de la flota. Los ingenieros lograron estabilizar la situación, pero el regreso se ha retrasado considerablemente.',
+    'exp_delay_3' => 'La expedición atravesó una zona de espacio distorsionado. Los motores sufrieron interferencias y la flota tardará más en volver de lo previsto.',
+    'exp_delay_4' => 'Un enjambre de partículas energéticas ralentizó el avance de la expedición. Los escudos resistieron, pero el trayecto de regreso será más largo.',
+    'exp_delay_5' => 'La flota quedó atrapada en una tormenta de radiación cósmica. Los sistemas de comunicación fallaron y el retorno se ha demorado hasta restablecer el control.',
 
     // advancement
     'exp_faster_1' => '',
@@ -61,19 +61,19 @@ $lang = [
     'exp_nothing_1' => 'Tu expedición hizo magníficas fotos de una super nova. No se obtuvo nada de la expedición pero al menos hay muchas posibilidades de ganar el concurso "Mejor Foto del Universo" este año.', // ok
     'exp_nothing_2' => 'Un ser de pura energía se aseguró que todos los miembros de la expedición solo miraran el hipnotizante patrón de los monitores. Cuando la mayoría de ellos se despejaron de nuevo, la expedición debía ser interrumpida ya que quedaba poco deuterio.', // ok
     'exp_nothing_3' => 'Bueno, ahora sabemos que esas 5 anomalías rojas no solo tienen efectos caóticos en los sitemas de navegación de la nave sino que también generan alucinaciones masivas en la tripulación. La expedición no trajo nada de vuelta.', // ok
-    'exp_nothing_4' => '',
-    'exp_nothing_5' => '',
-    'exp_nothing_6' => '',
-    'exp_nothing_7' => '',
+    'exp_nothing_4' => 'La expedición dedicó horas a analizar una nube de polvo cósmico. El resultado: alergias y estornudos, pero ningún recurso útil.',
+    'exp_nothing_5' => 'Nuestros exploradores encontraron una señal misteriosa que resultó ser el eco de una antigua canción marinera de BurSen. Tras horas de baile, no se obtuvo nada más.',
+    'exp_nothing_6' => 'La flota se perdió en una discusión filosófica sobre el sentido del universo. Al final, volvieron sin respuestas... ni recursos.',
+    'exp_nothing_7' => 'Un agujero negro resultó ser solo una sombra en el parabrisas. Tras una larga investigación, la expedición regresó con las manos vacías y una nueva teoría sobre la suciedad espacial.',
     'exp_nothing_8' => 'Probablemente la celebración del cumpleaños del capitán no debió hacerse en ese planeta desierto. Una fiebre terrible y desconocida causó que la gran mayoría de la tripulación de la expedición tuviera que estar en la enfermería durante el resto de la expedición. Debido a la escasez de personal la expedición fracasó.', // ok
-    'exp_nothing_9' => '',
-    'exp_nothing_10' => '',
-    'exp_nothing_11' => '',
-    'exp_nothing_12' => '',
-    'exp_nothing_13' => '',
-    'exp_nothing_14' => '',
-    'exp_nothing_15' => '',
-    'exp_nothing_16' => '',
+    'exp_nothing_9' => 'La expedición se entretuvo en un concurso de japoneses espaciales y sus preguntas sobre Japón. Tras horas de estudio, concluyeron que la respuesta es Japón y volvieron sin recursos.',
+    'exp_nothing_10' => 'La ruptura de una colonia de hormigas de una de las naves llevó a la flota a pensar que están siendo atacados por monstruosas hormigas espaciales. La tripulación se asustó y volvió sin nada útil.',
+    'exp_nothing_11' => 'El capitán decidió organizar una partida de ajedrez 4D intergaláctico. La expedición terminó sin hallazgos, pero con un nuevo campeón de ajedrez a bordo.',
+    'exp_nothing_12' => 'La flota se topó con una zona de silencio absoluto. Tras horas de meditación y yoga espacial, regresaron relajados pero sin ningún recurso.',
+    'exp_nothing_13' => 'El grupo de exploradores se perdió buscando la misteriorsa gran T. No paraban de decir: ¡MILLONES DE DÓLARES BAJO LA GRAN T!. Al final volvieron con las manos vacías.',
+    'exp_nothing_14' => 'La expedición encontró una señal que parecía importante, pero resultó ser publicidad de una compañía telefónica. Vuelven sin recursos, pero con un cupón de descuento y las mejores ofertas en telefonía intergaláctica.',
+    'exp_nothing_15' => 'La tripulación se fué por las ramas y dedicó el tiempo a buscar el WiFi universal. Tras muchos intentos, solo lograron conectar con memes antiguos y volvieron sin nada útil.',
+    'exp_nothing_16' => 'Debido a que un alien mencionó al capitán sobre el planeta de la chocolata, este se sumió en sus pensamientos durante todo el tiempo canturreando. Cuando volvió en sí preguntó a la tripulación ¿Hablábamos de chocolate?. Vuelven sin nada útil.',
 
     // lost ships
     'exp_lost_1' => '',
