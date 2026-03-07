a1dba665b8ed669c6c489e914bfc7fc1bc3ac5d6
Kas-Akari <37886456+Kas-Akari@users.noreply.github.com>
2026-03-07 14:21:42 +0100

    [Fix]Input of ships/defenses should only be number

--- resources/views/shipyard/shipyard_build_box.blade.php
+++ resources/views/shipyard/shipyard_build_box.blade.php
@@ -1,1 +1,1 @@
-<input type="text" name="fmenge[{{$item_id}}]" size="6" maxlength="6" value="0" tabindex="{{$tab_index}}">
+<input type="number" name="fmenge[{{$item_id}}]" size="6" maxlength="6" value="0" tabindex="{{$tab_index}}">
